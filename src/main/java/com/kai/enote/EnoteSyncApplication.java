f6709a8f37997a2240035cdacc62716d5aee10c7
DESKTOP-TTB5SSK\Kai <taiduong.264@gmail.com>
2023-07-26 13:50:14 +0700

    Create source

---
+++ src/main/java/com/kai/enote/EnoteSyncApplication.java
@@ -0,0 +1,13 @@
+package com.kai.enote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnoteSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnoteSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kai/enote/EnoteSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kai.enote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnoteSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
